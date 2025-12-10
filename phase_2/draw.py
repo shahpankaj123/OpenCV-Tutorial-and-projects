7931b10c7f542b47af1bcdde139bf795ee8521ac
shahpankaj123 <aaryanshah615@gmail.com>
2025-12-10 18:31:37 +0545

    drawing line ,circle ..etc on images

---
+++ phase_2/draw.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3),dtype='uint8')
+cv.imshow('blank image',blank)
+
+# 1. Paint the image a certain colour
+blank[200:300, 300:400] = 0,0,200
+cv.imshow('Green', blank)
+
+# 2. Draw a Rectangle
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
+
+# 2. Draw a circle
+cv.circle(blank ,(blank.shape[1]//2, blank.shape[0]//2) ,50 ,(255,0,0),thickness=-1)
+cv.imshow('circle', blank)
+
+cv.line(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),color=(30,50,200),thickness=5)
+cv.imshow("line",blank)
+
+# 5. Write text
+cv.putText(blank, 'Hello, my name is Pankaj!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (200,255,100), 2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
